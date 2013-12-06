df4ec8aa59462f97519c5b50fa04598b56e99a66
Arnav Gupta <championswimmer@aokp.co>
2013-12-06 10:15:18 +0530

    yuga: update AOKP build configuration
    Change-Id: I455e7949bc1df89f48f6d9f0714b6d696600bff0
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

---
+++ aokp.mk
@@ -0,0 +1,14 @@
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Inherit AOSP device configuration for yuga
+$(call inherit-product-if-exists, device/sony/yuga/full_yuga.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603_1270-4975 BUILD_FINGERPRINT=Sony/C6603_1270-4975/C6603:4.1.2/10.1.A.1.434/Tfp_rw:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.1.2 10.1.A.1.434 Tfp_rw test-keys"
+
+#skip asserts for now
+TARGET_OTA_ASSERT_SKIP := true
+
